2d2e18b4f9466339b855bca7ccf456bcd998f115
kadjialan <alankadji7@gmail.com>
2022-12-09 14:57:08 +0100

    linter resolution

--- asserts/styles/script/knapsack.js
+++ asserts/styles/script/knapsack.js
@@ -1,23 +1,23 @@
-const addButton = document.querySelector('.bt1')
-const maxweight = document.querySelector('.weight')
-const holder = document.getElementById('knapsack_bag')
-const sum = document.getElementById('total_sum')
-const selectOptions = document.getElementsByTagName('select')
-const doneButton = document.querySelector('.bt2')
-const selected = []
-const count = 0
-const sumOfWeights = 0
-const sumOfValue = 0
-const itemBox = [
-    { name: 'cement', weight: 50, value: 35 },
-    { name: 'stones', weight: 60, value: 13 },
-    { name: 'sand', weight: 100, value: 12 },
-    { name: 'zinc', weight: 30, value: 10 },
-    { name: 'nails', weight: 7, value: 75 },
-    { name: 'saw', weight: 1, value: 10 },
-    { name: 'wood', weight: 75, value: 72 },
-    { name: 'axe', weight: 7, value: 12 },
-    { name: 'hammer', weight: 5, value: 15 },
+let addButton = document.querySelector('.bt1')
+let maxweight = document.querySelector('.weight')
+let holder = document.getElementById('knapsack-bag')
+let sum = document.getElementById('total-sum')
+let selectOptions = document.getElementsByTagName("select")
+let doneButton = document.querySelector(".bt2")
+let selected = []
+let count;
+let sumOfWeights = 0
+let sumOfValue = 0
+let itemBox = [
+    { name: "cement", weight: 50, value: 35 },
+    { name: "stones", weight: 60, value: 13 },
+    { name: "sand", weight: 100, value: 12 },
+    { name: "zinc", weight: 30, value: 10 },
+    { name: "nails", weight: 7, value: 75 },
+    { name: "saw", weight: 1, value: 10 },
+    { name: "wood", weight: 75, value: 72 },
+    { name: "axe", weight: 7, value: 12 },
+    { name: "hammer", weight: 5, value: 15 },
 ]
 
 function reset() {
@@ -26,12 +26,12 @@ function reset() {
 
 function dataCollector() {
     count = 0;
-    const inputVal = maxweight.value;
+    let inputVal = maxweight.value;
 
 
-    addButton.addEventListener('click', () => {
-        for (const i = 0; i < selectOptions.length; i++) {
-            const currrentOption = selectOptions[i]
+    addButton.addEventListener("click", () => {
+        for (let i = 0; i < selectOptions.length; i++) {
+            let currrentOption = selectOptions[i]
             console.log(count)
             sumOfWeights += itemBox[count].weight;
             sumOfValue += itemBox[count].value
